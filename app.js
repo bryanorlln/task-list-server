adfb8be2ea68680f8d55897a7ae9e45ec03c8bbb
bryanorlln <bryandavidvilla@hotmail.com>
2023-11-15 23:55:33 -0600

    project-5 APIREST

--- app.js
+++ app.js
@@ -1,76 +1,89 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
 const app = express();
+const bodyParser = require('body-parser');
 
-dotenv.config(); // Cargar variables de entorno desde el archivo .env
+app.use(bodyParser.json());
 
-// Middleware para validar métodos HTTP válidos
-const validateMethod = (req, res, next) => {
-    if (req.method !== 'GET' && req.method !== 'POST' && req.method !== 'PUT' && req.method !== 'DELETE') {
-        return res.status(405).json({ error: 'Método HTTP no permitido' });
-    }
-    next();
-};
-
-// Middleware para verificar el token JWT en las rutas protegidas
-const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(401).json({ error: 'Acceso no autorizado: token no proporcionado' });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ error: 'Token inválido' });
-        }
-        // Si el token es válido, se puede continuar con la solicitud
-        req.user = decoded;
-        next();
-    });
-};
-
-app.use(express.json()); // Middleware para manejar solicitudes JSON
-app.use(validateMethod); // Aplicar el middleware de método a nivel de aplicación
-
-// Array de usuarios predefinidos (esto podría provenir de una base de datos)
-const predefinedUsers = [
-    { username: 'usuario1', password: 'contraseña1' },
-    { username: 'usuario2', password: 'contraseña2' },
-    // ... otros usuarios
+let tasks = [
+    { id: 1, title: 'Hacer la compra', completed: false },
+    { id: 2, title: 'Estudiar para el examen', completed: true },
+    { id: 3, title: 'Hacer ejercicio', completed: false }
 ];
+let nextTaskId = 4;
 
-// Ruta para autenticación y creación del token JWT
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    const user = predefinedUsers.find(
-        (u) => u.username === username && u.password === password
-    );
-
-    if (!user) {
-        return res.status(401).json({ error: 'Credenciales inválidas' });
-    }
-
-    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
-        expiresIn: '1h' // Establecer la expiración del token
-    });
-
-    res.json({ token });
+// Obtener todas las tareas
+app.get('/tasks', (req, res) => {
+    res.json({ tasks });
 });
 
-// Ruta protegida que requiere token JWT para acceder
-app.use('/protected', verifyToken, (req, res) => {
-    res.json({ message: 'Acceso permitido a la ruta protegida', user: req.user });
+// Crear una nueva tarea
+app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+
+    if (!title) {
+        return res.status(400).json({ error: 'Se requiere un título para la tarea' });
+    }
+
+    const newTask = { id: nextTaskId, title, completed: false };
+    nextTaskId++;
+    tasks.push(newTask);
+
+    res.status(201).json({ task: newTask });
+});
+
+// Obtener una sola tarea por su ID
+app.get('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(task => task.id === taskId);
+
+    if (!task) {
+        return res.status(404).json({ error: 'Tarea no encontrada' });
+    }
+
+    res.json({ task });
+});
+
+// Actualizar una tarea por su ID
+app.put('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const { title, completed } = req.body;
+
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({ error: 'Tarea no encontrada' });
+    }
+
+    if (title) {
+        tasks[taskIndex].title = title;
+    }
+
+    if (completed !== undefined) {
+        tasks[taskIndex].completed = completed;
+    }
+
+    res.json({ task: tasks[taskIndex] });
+});
+
+// Eliminar una tarea por su ID
+app.delete('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    tasks = tasks.filter(task => task.id !== taskId);
+    res.json({ message: 'Tarea eliminada exitosamente' });
+});
+
+// Listar tareas completas e incompletas
+app.get('/tasks/completed', (req, res) => {
+    const completedTasks = tasks.filter(task => task.completed);
+    res.json({ completedTasks });
+});
+
+app.get('/tasks/incomplete', (req, res) => {
+    const incompleteTasks = tasks.filter(task => !task.completed);
+    res.json({ incompleteTasks });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor en http://localhost:${PORT}`);
 });
-
-
-//Para correrlo (npm run dev) y ingresar en postman las rutas.
